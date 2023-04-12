3f41eb59a73ee67e25b824fed1f8e1e65e5e4711
Michelle <msbeaule@gmail.com>
2023-04-11 21:59:05 -0700

    added test strings

---
+++ test.py
@@ -0,0 +1,10 @@
+test_strings = [
+    "test [[1 link]] more text [[1.2 another link]] text text",
+    "text [[2 link]] text",
+    "text [[3 link|this is the text for the link]] text",
+    "```\
+    print('[[hello world]]')\
+    ```",
+    "[[]]",
+    "test [[link | custom name of link]]",
+]
