2519eeab72e2719923bda62c8ccd8b419b11a6d2
Michelle <msbeaule@gmail.com>
2023-04-12 19:29:48 -0700

    put the functions and variables into a class

--- main.py
+++ main.py
@@ -5,66 +5,70 @@ import glob as g
 import os
 import frontmatter
 
-markdown_files = []
-number_of_posts_copied_over = 0
+class Obsidian_to_Hugo:
 
-def remove_exists_hugo_posts():
-    try:
-        rmtree(config.hugo_posts_path)
-    except: pass
-    # adds the folder back as it's deleted above
-    os.makedirs(config.hugo_posts_path)
+    def __init__(self):
+        self.markdown_files = []
+        self.number_of_posts_copied_over = 0
 
-def find_all_markdown_files_in_obsidian():
-    for folder in config.obsidian_blog_folders:
-        path = config.obsidian_vault_path + "/" + folder
-        # print(path)
-        markdown_files.extend(g.glob(path + "/*.md"))
-
-def _replace_links_with_hugo_syntax(match_obj):
-    if match_obj.group(1) is not None:
-        link = match_obj.group(1).replace("[[", "").replace("]]", "")
-        # print(match_obj.group())
-
-        if link.find("|") >= 0:
-            link_text = link.split("|", 1)
-            return '[' + link_text[1].strip() + ']({{< ref "' + link_text[0].strip() +'" >}})'
-        
-        return '{{< ref "' + link + '" >}}'
-
-def replace_links(filedata) -> str:
-    return re.sub(r"(\[\[.+?\]\])", _replace_links_with_hugo_syntax, filedata)
-
-def replace_links_in_files_and_copy_files_to_hugo():
-    for file_path in markdown_files:
-        # Read in the file
-        with open(file_path, 'r') as file:
-            filedata = file.read()
-
-        # load the yaml frontmatter
-        yaml = frontmatter.loads(filedata)
-        
-        # check if blog post is still a draft, if it is skip over it and don't copy it over
+    def remove_existing_hugo_posts(self):
         try:
-            if yaml["draft"] is True:
+            rmtree(config.hugo_posts_path)
+        except: pass
+        # adds the folder back as it's deleted above
+        os.makedirs(config.hugo_posts_path)
+
+    def find_all_markdown_files_in_obsidian(self):
+        for folder in config.obsidian_blog_folders:
+            path = config.obsidian_vault_path + "/" + folder
+            # print(path)
+            self.markdown_files.extend(g.glob(path + "/*.md"))
+
+    def _replace_links_with_hugo_syntax(self, match_obj):
+        if match_obj.group(1) is not None:
+            link = match_obj.group(1).replace("[[", "").replace("]]", "")
+            # print(match_obj.group())
+
+            if link.find("|") >= 0:
+                link_text = link.split("|", 1)
+                return '[' + link_text[1].strip() + ']({{< ref "' + link_text[0].strip() +'" >}})'
+            
+            return '{{< ref "' + link + '" >}}'
+
+    def replace_links(self, filedata) -> str:
+        return re.sub(r"(\[\[.+?\]\])", self._replace_links_with_hugo_syntax, filedata)
+
+    def replace_links_in_files_and_copy_files_to_hugo(self):
+        for file_path in self.markdown_files:
+            # Read in the file
+            with open(file_path, 'r') as file:
+                filedata = file.read()
+
+            # load the yaml frontmatter
+            yaml = frontmatter.loads(filedata)
+            
+            # check if blog post is still a draft, if it is skip over it and don't copy it over
+            try:
+                if yaml["draft"] is True:
+                    continue
+            except KeyError:
+                print("ERROR: " + file_path + " doesn't have 'draft' yaml key, file not copied over")
                 continue
-        except KeyError:
-            print("ERROR: " + file_path + " doesn't have 'draft' yaml key, file not copied over")
-            continue
 
-        # Replace the target string
-        filedata = replace_links(filedata)
+            # Replace the target string
+            filedata = self.replace_links(filedata)
 
-        # Write the file out again
-        with open(config.hugo_posts_path + "/" + os.path.basename(file_path), 'w') as file:
-            file.write(filedata)
-            number_of_posts_copied_over += 1
+            # Write the file out again
+            with open(config.hugo_posts_path + "/" + os.path.basename(file_path), 'w') as file:
+                file.write(filedata)
+                self.number_of_posts_copied_over += 1
 
-remove_exists_hugo_posts()
+obsidian_to_hugo = Obsidian_to_Hugo()
 
-find_all_markdown_files_in_obsidian()
-# print(markdown_files)
+obsidian_to_hugo.remove_existing_hugo_posts()
 
-replace_links_in_files_and_copy_files_to_hugo()
+obsidian_to_hugo.find_all_markdown_files_in_obsidian()
 
-print("Number of posts successfully copied over to Hugo: " + str(number_of_posts_copied_over))
+obsidian_to_hugo.replace_links_in_files_and_copy_files_to_hugo()
+
+print("Number of posts successfully copied over to Hugo: " + str(obsidian_to_hugo.number_of_posts_copied_over))
