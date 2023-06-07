09623ff897510ad901567466efe9a1dfa938aceb
Michelle <msbeaule@gmail.com>
2023-06-07 11:20:48 -0700

    now can watch the folder for changes in markdown files, default set to checking every 10 seconds

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import config
 from shutil import rmtree
-import re
+import re, os, sys, time
 import glob as g
-import os
 import frontmatter
 from enum import Enum
+
+import argparse
 
 class PostStatus(Enum):
     IS_DRAFT = 1
@@ -31,7 +32,7 @@ class Obsidian_to_Hugo:
         for folder in config.obsidian_blog_folders:
             path = config.obsidian_vault_path + "/" + folder
             # print(path)
-            self.markdown_files.extend(g.glob(path + "/*.md"))
+            return g.glob(path + "/*.md")
 
     def _replace_links_with_hugo_syntax(self, match_obj):
         if match_obj.group(1) is not None:
@@ -44,7 +45,7 @@ class Obsidian_to_Hugo:
             
             return '{{< ref "' + link + '" >}}'
 
-    def replace_links(self, filedata) -> str:
+    def _replace_links(self, filedata) -> str:
         return re.sub(r"(\[\[.+?\]\])", self._replace_links_with_hugo_syntax, filedata)
 
     def _is_post_a_draft(self, filedata) -> PostStatus:
@@ -65,14 +66,14 @@ class Obsidian_to_Hugo:
             file.write(filedata)
             self.number_of_posts_copied_over += 1
 
-    def replace_links_in_files_and_copy_files_to_hugo(self):
-        for file_path in self.markdown_files:
+    def replace_links_in_files_and_copy_files_to_hugo(self, markdown_files):
+        for file_path in markdown_files:
             # Read in the file
             with open(file_path, 'r') as file:
                 filedata = file.read()
 
             # Replace the target string
-            filedata = self.replace_links(filedata)
+            filedata = self._replace_links(filedata)
 
             draft_status = self._is_post_a_draft(filedata)
 
@@ -83,6 +84,7 @@ class Obsidian_to_Hugo:
 
             if draft_status == PostStatus.DRAFT_YAML_KEY_DOESNT_EXIST:
                 print("WARNING: post doesn't have 'draft' frontmatter key, post treated as published by Hugo: " + file_path)
+                sys.stdout.flush() # so print() works on git bash
 
             # Write the file out again
             with open(config.hugo_posts_path + "/" + os.path.basename(file_path), 'w') as file:
@@ -90,14 +92,55 @@ class Obsidian_to_Hugo:
                 self.number_of_posts_copied_over += 1
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+                    prog='Obsidian to Hugo',
+                    description='Copies over Obsidian .md files to Hugo.',
+                    epilog='Look at README for more info')
+
+    parser.add_argument('-w', '--watch', nargs="?", const=True, default=False,
+                        help="Copies over everything but then watches for changes (when watching does not delete files in Hugo, only replaces ones that change)")
+    args = parser.parse_args()
+
     obsidian_to_hugo = Obsidian_to_Hugo()
+    markdown_files = []
 
     obsidian_to_hugo.remove_existing_hugo_posts()
 
     obsidian_to_hugo.recreate_hugo_folders()
 
-    obsidian_to_hugo.find_all_markdown_files_in_obsidian()
+    markdown_files = obsidian_to_hugo.find_all_markdown_files_in_obsidian()
 
-    obsidian_to_hugo.replace_links_in_files_and_copy_files_to_hugo()
+    obsidian_to_hugo.replace_links_in_files_and_copy_files_to_hugo(markdown_files)
 
     print("Number of posts successfully copied over to Hugo: " + str(obsidian_to_hugo.number_of_posts_copied_over))
+    sys.stdout.flush() # so print() works on git bash
+
+    if args.watch:
+        print("Watching for changes...")
+        sys.stdout.flush() # so print() works on git bash
+
+        print("Press Ctrl C to quit")
+        sys.stdout.flush() # so print() works on git bash
+
+        time_to_compare_to = time.time()
+        while True:
+            obsidian_to_hugo.number_of_posts_copied_over = 0
+
+            markdown_files = obsidian_to_hugo.find_all_markdown_files_in_obsidian()
+
+            modified_markdown_files = []
+
+            for file in markdown_files:
+                file_mod_time = os.path.getmtime(file)
+                if file_mod_time > time_to_compare_to:
+                    modified_markdown_files.append(file)
+            
+            if len(modified_markdown_files) > 0:
+                obsidian_to_hugo.replace_links_in_files_and_copy_files_to_hugo(modified_markdown_files)
+
+                print("Number of posts successfully copied over to Hugo: " + str(obsidian_to_hugo.number_of_posts_copied_over))
+                sys.stdout.flush() # so print() works on git bash
+
+
+            time_to_compare_to = time.time()
+            time.sleep(10)
